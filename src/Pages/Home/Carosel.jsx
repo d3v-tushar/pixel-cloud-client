2446b3acf741dd4b135106397b4a952f7fb1b5bb
d3v-tushar <d3v.tushar@gmail.com>
2023-01-10 17:59:10 +0600

    Swiper JS Slider Added

--- src/Pages/Home/Carosel.jsx
+++ src/Pages/Home/Carosel.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import logo from './../../assets/logo.png';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
 
 const Carosel = ({slide}) => {
     const {image, id, prev, next} = slide;
   return (
-    <div id={`slide${id}`} className="carousel-item relative w-full">
+   <>
+    {/* <div id={`slide${id}`} className="carousel-item relative w-full">
         <div className='carousel-img w-full'>
         <img src={image} alt='' className="rounded-xl min-w-screen" />
         </div>
@@ -17,7 +25,33 @@ const Carosel = ({slide}) => {
             <a href={`#slide${prev}`} className="btn btn-circle mr-5">❮</a> 
             <a href={`#slide${next}`} className="btn btn-circle">❯</a>
             </div>
-        </div> 
+        </div>  */}
+
+        <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
+   </>
     
   );
 };
